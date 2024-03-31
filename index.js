aeebb9c6be1f8c5debb1f77dfec972b689552d01
AbdulSaheeb <saheebtkhaskheli@gmail.com>
2024-03-31 05:18:14 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// printing a Wellcome Message
+console.log("\n\tWellcome To \'saheebkhaskheli\' - CLI simple Calculator\n");
+// Asking Questions form users through Inquirer
+let answers = await inquirer.prompt([
+    { message: "Enter first Number", type: "number", name: "firstNumber" },
+    { message: "Enter second Number", type: "number", name: "secondNumber" },
+    {
+        message: "Select one Operator to perform Operayions",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtractions", "Multiplication", "division"],
+    },
+]);
+// Condtional statements If-Else
+if (answers.operator === "Addition") {
+    console.log(answers.firstNumber + answers.secondNumber);
+}
+else if (answers.operator === "Subtractions") {
+    console.log(answers.firstNumber - answers.secondNumber);
+}
+else if (answers.operator === "Multiplication") {
+    console.log(answers.firstNumber * answers.secondNumber);
+}
+else if (answers.operator === "division") {
+    console.log(answers.firstNumber / answers.secondNumber);
+}
+else {
+    console.log("Invalid Input");
+}
